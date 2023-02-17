d5fe347a825086e459343c2782ddf99b7607441b
Jussi-Tapio Lamminheimo <jusulamminheimo@gmail.com>
2023-02-17 22:11:26 +0200

    11.8

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,23 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "9c18022ef6907c9955a02ed9f3617303",
+  "assets/AssetManifest.json": "1233855f99c519e0ddd4ff8aeef8deac",
 "assets/assets/background.png": "9d95286e609be703b423409f43f23326",
 "assets/assets/front_background.png": "5ab37ee2c211d64cb1fcfe17584915f4",
-"assets/FontManifest.json": "ff609acbfca9625ac29a6e7d79e078e0",
+"assets/FontManifest.json": "07e473f6a35438dc401e23e5ff64f73a",
 "assets/fonts/BigShouldersDisplay-Bold.ttf": "ede3799127cbec0f20b461ba2eadee50",
 "assets/fonts/BigShouldersDisplay-ExtraBold.ttf": "185c5a8a9d8468a77a1f97d088a04007",
 "assets/fonts/BigShouldersDisplay-Regular.ttf": "1fe48df13b450f96904f68cc34d12965",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
+"assets/fonts/WorkSans-Black.ttf": "d174236ffb8ef7c2158395e4ee83e3b6",
+"assets/fonts/WorkSans-BlackItalic.ttf": "397e988b62638fa420f269108c81bedf",
+"assets/fonts/WorkSans-Bold.ttf": "f051cc0ed2761378e886727284cf3c05",
+"assets/fonts/WorkSans-ExtraBold.ttf": "749d400e25914e869b2cb9f554439eb4",
+"assets/fonts/WorkSans-ExtraLight.ttf": "08652b8358873ee1f44ac52b67b1c109",
+"assets/fonts/WorkSans-Light.ttf": "fac65b7a2b5c287d027e8ab9df15ff58",
+"assets/fonts/WorkSans-MediumItalic.ttf": "624fb450f2a020fbf2ff5ac9ce819735",
+"assets/fonts/WorkSans-Regular.ttf": "7d761a652f8e716f57f4352b0f4e6280",
+"assets/fonts/WorkSans-Thin.ttf": "69825d3bbd338a65e0ba517745d84be7",
 "assets/NOTICES": "c0b0436daecefc6fc947e475d81b4922",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
 "canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
@@ -22,9 +31,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "db7dc659aa4187674b8edc92eb8e645f",
-"/": "db7dc659aa4187674b8edc92eb8e645f",
-"main.dart.js": "a1078faf3315d4afcd3e51d8427335af",
+"index.html": "691cf56bde4d1a99eeb64a3910191679",
+"/": "691cf56bde4d1a99eeb64a3910191679",
+"main.dart.js": "9a937069ea2866d9a9ac43c1b0543845",
 "manifest.json": "9f78207f15c7900eca7fd79fd6b8bf0d",
 "version.json": "00a064811a1326ef49c71e9b8d36a984"
 };
