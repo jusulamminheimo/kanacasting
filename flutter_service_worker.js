dbcf415b1602eda49ca8cc6ebb02b284b1bdced2
Jussi-Tapio Lamminheimo <jusulamminheimo@gmail.com>
2023-03-01 10:55:09 +0200

    11.12

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,9 +31,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ba749f3791e508827dd4a43ca82b8eaa",
-"/": "ba749f3791e508827dd4a43ca82b8eaa",
-"main.dart.js": "2f5711e1d2a5ece95843bc3a1d0bbb32",
+"index.html": "2e66dcf4be3c819b4c9da34f189f2eb7",
+"/": "2e66dcf4be3c819b4c9da34f189f2eb7",
+"main.dart.js": "aa0dd8722802bb91327b6debab11d829",
 "manifest.json": "9f78207f15c7900eca7fd79fd6b8bf0d",
 "version.json": "00a064811a1326ef49c71e9b8d36a984"
 };
